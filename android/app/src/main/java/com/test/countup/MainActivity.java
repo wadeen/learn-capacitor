0a5b658dbb7490a67b7b070931110e8e965f4471
wadeen <meister.vvdo@gmail.com>
2024-04-27 22:22:17 +0900

    Add capacitor

---
+++ android/app/src/main/java/com/test/countup/MainActivity.java
@@ -0,0 +1,5 @@
+package com.test.countup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.test.countup",
+  appName: "CountUp",
+  webDir: "dist",
+};
+
+export default config;
